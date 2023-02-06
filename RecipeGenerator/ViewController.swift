344842363e7a97642f48826141b9e84fbcb08848
Shreemit Garimella <shreemit27@gmail.com>
2023-02-05 17:54:12 -0800

    Added iOS files

---
+++ RecipeGenerator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RecipeGenerator
+//
+//  Created by Shreemit Garimella on 2/3/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    // Testing 
+
+}
+
